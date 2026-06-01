820b6103bf67bb9510d81dc2650216b2ca9dcc2d
Sandeep Tangri <sandeeptangri@gmail.com>
2026-06-01 13:52:47 +0100

    Add payment, notification, and order microservices

---
+++ backend/order-service/src/main.ts
@@ -0,0 +1,11 @@
+﻿import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  const port = process.env.PORT || 3004;
+  await app.listen(port);
+  console.log('Order Service on port ' + port);
+}
+bootstrap();
